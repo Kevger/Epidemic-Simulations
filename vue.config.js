9880ac26e30aac3d2d0c01fc7a9d474a4fb6e592
Kevin German <kevger123@gmail.com>
2020-05-22 15:49:12 +0200

    disabled sourcemaps

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {
+    themeColor: "#FFFFFF",
+    msTileColor: "#FFFFFF",
+    manifestOptions: {
+      background_color: "#FFFFFF"
+    },
+    name: "Epidemic Simulations"
+  },
+
+  productionSourceMap: false
+};
